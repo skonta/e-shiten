99c6fe2abfc804ff4046e6c3d5c551c367e04918
Konta <skonta@open-quant.com>
2025-05-06 15:35:26 +0900

    aa

--- login.py
+++ login.py
@@ -1,10 +1,11 @@
 import requests
 import json
 from urllib.parse import quote
+from datetime import datetime
 
 data ={ 
     "p_no": "2",
-    "p_sd_date": "2025.01.07-12:11:35.000",
+    "p_sd_date":datetime.now().strftime("%Y.%m.%d-%H:%M:%S.%f")[:-3],
     "sCLMID": "CLMAuthLoginRequest",
     "sUserId": "hcw05193",  
     "sPassword": "Konta9323",
@@ -15,7 +16,7 @@ json_string =json.dumps(data)
 encoded_json_string = quote(json_string)
 
 
-url = f'https://demo-kabuka.e-shiten.jp/e_api_v4r5/auth/?{encoded_json_string}'
+url = f'https://demo-kabuka.e-shiten.jp/e_api_v4r6/auth/?{encoded_json_string}'
 
 
 response = requests.get(url)
